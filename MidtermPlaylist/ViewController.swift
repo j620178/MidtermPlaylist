7579852222e35bb62869e9f75c6e36f39cf6e495
littlema <j620178@gmail.com>
2019-08-23 09:30:28 +0800

    First Commit

---
+++ MidtermPlaylist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MidtermPlaylist
+//
+//  Created by littlema on 2019/8/23.
+//  Copyright © 2019 littema. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
